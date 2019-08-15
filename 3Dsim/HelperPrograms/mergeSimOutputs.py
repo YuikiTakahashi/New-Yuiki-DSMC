f917fa74858424a04025f61f24eab97139a81612
gabrielwoolls <gabrielwoolls@gmail.com>
2019-08-15 13:30:08 -0700

    Streamlining mergeSimOutputs.py, and adding flow n100.

--- 3Dsim/HelperPrograms/mergeSimOutputs.py
+++ 3Dsim/HelperPrograms/mergeSimOutputs.py
@@ -18,6 +18,7 @@ parser = ArgumentParser()
 parser.add_argument('-folder', '--directory')
 parser.add_argument('-fout', '--outfile')
 parser.add_argument('-fin', '--file_list', nargs='+', default=[])
+parser.add_argument('--patmax', dest='pattern', action='store', type=int, default=0)
 parser.add_argument('--app', dest='append', action='store_true', default=False)
 args=parser.parse_args()
 
@@ -25,14 +26,13 @@ DIRECTORY = args.directory
 OUTFILE = args.outfile
 INFILE_LIST = args.file_list
 APPEND = args.append
-
+PATMAX = args.pattern
 OUTFILE_FULL = DIRECTORY+'\\'+OUTFILE
 
 INFILE_LIST_FULL = []
-for inf in INFILE_LIST:
-    INFILE_LIST_FULL.append(DIRECTORY+'\\'+inf)
 
-print("Write to {} from {}, in directory {}".format(OUTFILE,INFILE_LIST,DIRECTORY))
+
+print("Write to {} from {}".format(OUTFILE,INFILE_LIST))
 print("Append is {}".format(APPEND))
 
 try:
@@ -40,8 +40,23 @@ try:
 except StopIteration:
     print("0 particles in {0}".format(OUTFILE))
 
-for f in INFILE_LIST:
-    print("Particles in {0}: {1}".format(f, count_particles(DIRECTORY+'\\'+f)))
+
+
+
+if PATMAX == 0:
+    for inf in INFILE_LIST:
+        INFILE_LIST_FULL.append(DIRECTORY+'\\'+inf)
+        print("Particles in {0}: {1}".format(inf, count_particles(DIRECTORY+'\\'+inf)))
+
+else: #then PATMAX is the number of files to try for using the template in -fin
+    template = INFILE_LIST[0]
+    for i in range(PATMAX):
+        infile = template.format(i+1)
+        INFILE_LIST_FULL.append(DIRECTORY+'\\'+infile)
+        print("Particles in {0}: {1}".format(infile, count_particles(DIRECTORY+'\\'+infile)))
+
+
+
 
 if APPEND==False:
     with open(OUTFILE_FULL, 'w+') as fout, fileinput.input(INFILE_LIST_FULL) as fin:
